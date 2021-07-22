fcaf27273a19724e865878c0390c619ebd1774d5
SarandaQevani20 <>
2021-07-22 12:57:03 +0200

    program.cs file- added if statement

--- Data Security Project-P2/ServerSide/ServerSide/Program.cs
+++ Data Security Project-P2/ServerSide/ServerSide/Program.cs
@@ -161,3 +161,63 @@ namespace ServerSide
                 StreamReader s4 = new StreamReader(ns);
                 StreamReader s5 = new StreamReader(ns);
                 StreamReader s6 = new StreamReader(ns);
+                if (Int32.Parse(x) == 1)
+                {
+
+                    //server.AcceptTcpClientAsync().Wait(TimeSpan.FromSeconds(200));
+
+                    Console.WriteLine("Enter your name,details,etc");
+                    string p1 = s1.ReadLine();
+                    Console.WriteLine(p1);
+                    string p2 = s1.ReadLine();
+                    Console.WriteLine(p2);
+                    string p3 = s1.ReadLine();
+                    Console.WriteLine(p3);
+                    string p4 = s1.ReadLine();
+                    Console.WriteLine(p4);
+                    string p5 = s1.ReadLine();
+                    Console.WriteLine(p5);
+                    string p6 = s1.ReadLine();
+                    Console.WriteLine(p6);
+
+                    Register(p1, p2, p4, p6, Int32.Parse(p5), Double.Parse(p3));
+                    Console.WriteLine("OK, Registration Successful.......");
+                }
+
+                else
+                {
+                    Console.WriteLine("enter the details");
+                    StreamReader sr1 = new StreamReader(ns);
+                    string xio = sr1.ReadLine();
+                    Console.WriteLine(xio);
+                    StreamReader sr24 = new StreamReader(ns);
+                    string y = sr24.ReadLine();
+                    Console.WriteLine(y);
+                    string DecyptedPass = HarshCode.RSADecrypt(y);
+                    string DecryptedUser = HarshCode.RSADecrypt(xio);
+                    string safepass = Class1.hashPassword(DecyptedPass).ToString();
+                    Console.WriteLine(safepass);
+                    //------------------------------------------------------------
+
+
+
+                    if (Password.Contains(safepass) || Name.Contains(DecryptedUser))
+                    {
+                        Console.WriteLine("Sign a Digital Doc ");
+                        HarshSign sender = new HarshSign();
+                        string BigB = Extraction(Super, DecryptedUser.Substring(0, 4));
+                        Console.WriteLine(BigB);
+                        DigitalSignatureResult res = sender.BuildSignedMessage(BigB);
+                        Console.WriteLine(xio.Substring(0, 4));
+                        Console.WriteLine("cipher" + res.CipherText);
+                        Console.WriteLine("cSigner" + res.SignatureText);
+                        StreamWriter signciher = new StreamWriter(ns);
+                        StreamWriter Signwrite = new StreamWriter(ns);
+                        signciher.WriteLine(res.CipherText);
+                        signciher.Flush();
+                        Signwrite.WriteLine(res.SignatureText);
+                        Signwrite.Flush();
+                        signciher.Close();
+                        Signwrite.Close();
+
+                    }
