f7189652e81a591d1ef516898c9462ef511b2201
VeRoNa-KrAsNiQi <75127287+VeRoNa-KrAsNiQi@users.noreply.github.com>
2021-07-22 11:47:46 +0200

    Gjenerimi i celsave permes rsa

--- Data Security Project-P2/ServerSide/ServerSide/generator.cs
+++ Data Security Project-P2/ServerSide/ServerSide/generator.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExpressEncription;
+namespace ServerSide
+{
+    class generator
+    {
+        static void main (string []args)
+        {
+
+            ExpressEncription.RSAEncription.MakeKey(@"C:\Users\freec\Desktop\Data Security Project\public.key", @"C:\Users\freec\Desktop\Data Security Project\private.key");
+        }
+    }
+}
