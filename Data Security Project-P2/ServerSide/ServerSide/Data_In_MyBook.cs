e1b241c4be223090da41d239366ab6d6185af044
sara.haliti@student.uni-pr.edu <sara.haliti@student.uni-pr.edu>
2021-07-22 12:16:49 +0200

    File tjeter data in my books

--- Data Security Project-P2/ServerSide/ServerSide/Data_In_MyBook.cs
+++ Data Security Project-P2/ServerSide/ServerSide/Data_In_MyBook.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSide
+{
+   public class Data_In_MyBook
+    {
+
+        public string name { get; set; }
+        public string Password { get; set; }
+        public string month { get; set; }
+        public string invoice { get; set; }
+        public double euro { get; set; }
+        public  int year { get; set; }
+
+    }
+}
